b585ff2787a6eac852ca4728c6f25a6653eb5667
Joana Palero <joana.palero24@gmail.com>
2019-02-27 23:36:17 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
+
+  #This will display the Products#index (the root route)
   def index
   end
 
+  #This will post the products to add them to the cart.
   def add
-    session[:cart] << params[:product]
-    render :index
+    cart << params[:product]
+    redirect_to root_path
   end
+
 end
